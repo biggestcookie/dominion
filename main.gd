bbbd32a8ba3a51900da4940a961c25f25ac0d154
biggestcookie <5748299+biggestcookie@users.noreply.github.com>
2024-12-21 21:18:34 -0500

    Fix calculation

--- main.gd
+++ main.gd
@@ -17,45 +17,23 @@ var press_timing := Array()
 var function: Function
 
 func _ready() -> void:
-	timer.timeout.connect(func() -> void:
-		print("Timer ended")
-		is_active = false
-		is_displaying_results = true
-		results.show()
-		label.hide()
-		results_timer.start()
-		presses_label.text = "Presses: %s" % [str(presses)]
-		plot()
-	)
+	timer.timeout.connect(show_results)
 	results_timer.timeout.connect(func() -> void:
 		print("Results timer ended")
 		is_displaying_results = false
 		results_label.text = "Start mashing to try again"
 	)
-
-	# Change this back
 	label.show()
 	results.hide()
-	# plot()
 
 
 func _physics_process(_delta: float) -> void:
 	if !is_active and !is_displaying_results:
 		if Input.is_action_just_released("mash"):
-			# Start mashing
-			print("Timer started")
-			is_active = true
-			label.show()
-			results.hide()
-			timer.start()
-			presses = 0
-			timing_index = 0
-			press_timing.clear()
-			press_timing.resize(600)
-			press_timing.fill(false)
+			start_mashing()
 		return
 	elif is_displaying_results:
-		# Wait for results
+		# Update results timer
 		results_label.text = str(int(results_timer.time_left + 1))
 		return
 
@@ -66,11 +44,33 @@ func _physics_process(_delta: float) -> void:
 	timing_index += 1
 
 
+func start_mashing() -> void:
+	print("Timer started")
+	is_active = true
+	label.show()
+	results.hide()
+	timer.start()
+	presses = 0
+	timing_index = 0
+	press_timing.clear()
+	press_timing.resize(600)
+	press_timing.fill(false)
+
+
+func show_results() -> void:
+	print("Timer ended")
+	is_active = false
+	is_displaying_results = true
+	results.show()
+	label.hide()
+	results_timer.start()
+	presses_label.text = "Presses: %s" % [str(presses)]
+	plot()
+
+
 func plot() -> void:
 	var plots = calculate_velocity(press_timing)
 	
-	# Let's customize the chart properties, which specify how the chart
-	# should look, plus some additional elements like labels, the scale, etc...
 	var cp: ChartProperties = ChartProperties.new()
 	cp.colors.frame = Color("#161a1d")
 	cp.colors.background = Color.TRANSPARENT
@@ -78,15 +78,13 @@ func plot() -> void:
 	cp.colors.ticks = Color("#283442")
 	cp.colors.text = Color.WHITE_SMOKE
 	cp.draw_bounding_box = false
-	cp.title = "Presses per second"
+	cp.title = "Press velocity over time"
 	cp.x_label = "Time (seconds)"
-	cp.y_label = "Presses"
+	cp.y_label = "Presses per second"
 	cp.x_scale = 10
 	cp.y_scale = plots.y.max() + 1
-	cp.interactive = true # false by default, it allows the chart to create a tooltip to show point values
-	# and interecept clicks on the plot
-	
-	# Let's add values to our functions
+	cp.interactive = true
+
 	function = Function.new(
 		plots.x, plots.y, "Number of presses",
 		{
@@ -99,16 +97,15 @@ func plot() -> void:
 	
 	chart.set_x_domain(0, 10)
 	chart.set_y_domain(0, plots.y.max() + 1)
-	# Now let's plot our data
+
 	chart.plot([function], cp)
-	
-	# Uncommenting this line will show how real time data plotting works
 	set_process(false)
 
 
 func calculate_velocity(data: Array) -> Dictionary:
-	var num_segments := 10
-	var segment_length := data.size() / (num_segments)
+	var num_segments := 20
+	var segment_length := data.size() / num_segments
+	var segment_time := (1.0 / 60.0) * segment_length
 	var x_points := []
 	var y_points := []
 	
@@ -119,6 +116,7 @@ func calculate_velocity(data: Array) -> Dictionary:
 		for j in range(start, end):
 			if data[j]:
 				button_presses += 1
-		x_points.append((1.0 / 60) * end)
-		y_points.append(button_presses)
+		x_points.append((1.0 / 60.0) * end)
+		y_points.append(button_presses / segment_time)
+
 	return {"x": x_points, "y": y_points}
