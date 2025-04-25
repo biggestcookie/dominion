99939ed9da7e46c7dd5ca9e6c7b1c354d2bb0a0e
biggestcookie <5748299+biggestcookie@users.noreply.github.com>
2025-04-25 09:57:42 -0400

    wip timers

--- scenes/gameplay/gameplay.gd
+++ scenes/gameplay/gameplay.gd
@@ -7,4 +7,14 @@ extends Node2D
 # Set max time for 60 seconds
 
 func _ready() -> void:
-	pass
+	Events.game_state_changed.connect(on_game_state_changed)
+
+
+func on_game_state_changed(new_state: Main.GameState) -> void:
+	if new_state != Main.GameState.GAMEPLAY:
+		return
+	start_gameplay()
+
+
+func start_gameplay() -> void:
+	countdown_label
