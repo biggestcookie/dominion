ace376b169c14cd3774141ef325df12fefa477ac
bigcookie <5748299+biggestcookie@users.noreply.github.com>
2025-04-28 15:44:13 -0400

    working table, i think

--- components/table/table_component.gd
+++ components/table/table_component.gd
@@ -1,1 +1,17 @@
+class_name TableComponent
 extends VBoxContainer
+
+@onready var header: TableRow = $TableHeader
+@onready var scroll_container: ScrollContainer = $ScrollContainer
+
+var table_headers: Array[String] = []
+var table_data: Array[Array] = []
+
+func _ready() -> void:
+	# Initialize the table with headers and data
+	header.row_data = table_headers
+
+	for row_data in table_data:
+		var row: TableRow = preload("res://components/table/table_row.tscn").instantiate()
+		row.update_table_row(row_data)
+		scroll_container.add_child(row)
--- components/table/table_row.gd
+++ components/table/table_row.gd
@@ -1,22 +1,31 @@
 class_name TableRow
 extends HBoxContainer
 
-var header_titles: Array[String] = []
+@export var is_header: bool = false
+var row_data: Array[String] = []:
+	set(data):
+		update_table_row(data)
 
-func _ready() -> void:
-	for index in header_titles.size():
-		var title := header_titles[index]
+
+func update_table_row(data: Array[String]) -> void:
+	row_data = data # Set the row_data to the provided data
+	# Delete all children
+	for child in get_children():
+		remove_child(child)
+		child.queue_free()
+
+	for index in range(row_data.size()):
+		var title := row_data[index]
 		var margin := MarginContainer.new()
 		margin.add_theme_constant_override("margin_right", 60)
 		
 		var label := Label.new()
 		label.text = title
-		label.clip_text = true 
+		label.clip_text = true
 		if index == 0:
+			margin.add_theme_constant_override("margin_bottom", 10)
 			label.size_flags_horizontal = SIZE_EXPAND_FILL
-
-		# Set label settings
-		# If header, add 1px outline
+			label.add_theme_constant_override("outline_width", 1)
 
 		margin.add_child(label)
 		add_child(margin)
